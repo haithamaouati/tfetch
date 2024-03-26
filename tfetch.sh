1f759511297ead438de50b4abfabdf601b90ad45
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2024-03-26 23:52:37 +0100

    Create tfetch.sh

---
+++ tfetch.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Author: Haitham Aouati
+# GitHub: github.com/haithamaouati
+# A super tiny system information fetch script written in BASH
+
+# Custom
+CLEAR="\e[0m"
+BOLD="\e[1m"
+UNDERLINE="\e[4m"
+
+clear
+
+# Username
+username=$(whoami)
+
+# Hostname
+hostun=$(hostname)
+
+# OS
+os=$(uname -o)
+
+# Host
+host=$(uname -m)
+
+# Kernel version
+kernel=$(uname -r)
+
+# Uptime
+uptime=$(uptime -p)
+
+# Number of installed packages
+pkgs=$(dpkg -l | grep -c "^ii")
+
+# Memory (in MB)
+memory=$(free -m | awk '/Mem/{print $2}')
+
+# Displaying information
+echo -e "\n"
+echo -e "     ___    ${BOLD}$username@$hostun"
+echo -e "    (.· |   ${BOLD}os${CLEAR}     $os"
+echo -e "    (<> |   ${BOLD}host${CLEAR}   $host"
+echo -e "   / __  \  ${BOLD}kernel${CLEAR} $kernel"
+echo -e "  ( /  \ /| ${BOLD}uptime${CLEAR} $uptime"
+echo -e " _/\ __)/_) ${BOLD}pkgs${CLEAR}   $pkgs"
+echo -e " \/-____\/  ${BOLD}memory${CLEAR} ${memory}MB\n"
+
+# Author
+echo -e "${BOLD}tfetch ${CLEAR}by Haitham Aouati"
+echo -e "${BOLD}GitHub:${CLEAR} ${UNDERLINE}github.com/haithamaouati${CLEAR}\n"
+
+# Exit
+exit 0
