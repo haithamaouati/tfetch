b765c43df7ebf713aedf3a0825af760fb8933657
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2025-04-30 02:21:44 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p "$HOME/.local/bin"
-curl -fsSL https://raw.githubusercontent.com/haithamaouati/tfetch/main/tfetch.sh -o "$HOME/.local/bin/tfetch"
-chmod +x "$HOME/.local/bin/tfetch"
-echo "Installed to ~/.local/bin/tfetch"
